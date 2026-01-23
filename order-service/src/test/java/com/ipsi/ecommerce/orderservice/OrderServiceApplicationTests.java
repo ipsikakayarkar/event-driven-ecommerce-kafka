522da241b3076b34b806e469701ad57f2feaf288
ipsikakayarkar <ipsikakayarkar1894@gmail.com>
2026-01-22 17:42:42 -0800

    initial project stage

---
+++ order-service/src/test/java/com/ipsi/ecommerce/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsi.ecommerce.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
